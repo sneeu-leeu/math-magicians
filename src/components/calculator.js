ac020c92be2e638cc35f6e309a94fbb2a674f0c6
sneeu-leeu <sa070197@gmail.com>
2021-08-16 22:19:34 +0200

    Update button structure

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -5,27 +5,32 @@ class Calculator extends React.Component {
   render() {
     return (
       <div id="calculator">
-        <input className="" value="0" />
-        <div className="buttons">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button className="" type="button">/</button>
-          <button type="button">7</button>
-          <button type="button">8</button>
-          <button type="button">9</button>
-          <button className="" type="button">x</button>
-          <button type="button">4</button>
-          <button type="button">5</button>
-          <button type="button">6</button>
-          <button className="" type="button">-</button>
-          <button type="button">1</button>
-          <button type="button">2</button>
-          <button type="button">3</button>
-          <button className="" type="button">+</button>
-          <button id="nill-button" type="button">0</button>
-          <button type="button">.</button>
-          <button className="" type="button">=</button>
+        <input className="cal-val" value="0" />
+        <div className="buttons grid-box">
+
+          <button className="grid-item" type="button">+/-</button>
+          <button className="grid-item" type="button">%</button>
+          <button className="grid-item" type="button">AC</button>
+          <button className="grid-item bg-orange" type="button">/</button>
+
+          <button className="grid-item" type="button">7</button>
+          <button className="grid-item" type="button">8</button>
+          <button className="grid-item" type="button">9</button>
+          <button className="grid-item bg-orange" type="button">x</button>
+
+          <button className="grid-item" type="button">4</button>
+          <button className="grid-item" type="button">5</button>
+          <button className="grid-item" type="button">6</button>
+          <button className="grid-item bg-orange" type="button">-</button>
+
+          <button className="grid-item" type="button">1</button>
+          <button className="grid-item" type="button">2</button>
+          <button className="grid-item" type="button">3</button>
+          <button className="grid-item bg-orange" type="button">+</button>
+
+          <button className="grid-item nill-btn" type="button">0</button>
+          <button className="grid-item" type="button">.</button>
+          <button className="grid-item bg-orange" type="button">=</button>
         </div>
       </div>
     );
