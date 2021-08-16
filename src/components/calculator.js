db5175db7f22f7c8a1e5db9a42664bc9825e3b32
sneeu-leeu <sa070197@gmail.com>
2021-08-16 22:05:01 +0200

    Revert "Add div tags to help order"
    This reverts commit 2ea7ea011a93617131dd656458c4a3b9630b6cee.

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -6,36 +6,26 @@ class Calculator extends React.Component {
     return (
       <div id="calculator">
         <input className="" value="0" />
-        <div className="buttons grid-box">
-          <div className="buttons-row">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button">/</button>
-          </div>
-          <div className="buttons-row">
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button">x</button>
-          </div>
-          <div className="buttons-row">
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button">-</button>
-          </div>
-          <div className="buttons-row">
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button">+</button>
-          </div>
-          <div className="buttons-row">
-            <button id="nill-button" type="button">0</button>
-            <button type="button">.</button>
-            <button type="button">=</button>
-          </div>
+        <div className="buttons">
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button className="" type="button">/</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button className="" type="button">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button className="" type="button">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button className="" type="button">+</button>
+          <button id="nill-button" type="button">0</button>
+          <button type="button">.</button>
+          <button className="" type="button">=</button>
         </div>
       </div>
     );
