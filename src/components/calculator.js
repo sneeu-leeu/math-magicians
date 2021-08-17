8e16e36cc821eef6c8b7301894f243d76a4a4179
sneeu-leeu <sa070197@gmail.com>
2021-08-17 09:58:22 +0200

    Change class to function calculator and add button clicked es6

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,66 +1,57 @@
 /* eslint-disable react/prefer-stateless-function */
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import '../logic/operate';
 
-class Calculator extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      operation: null,
-      next: null,
-      total: null,
-    };
-    this.buttonClicked = this.buttonClicked.bind(this);
-  }
-
-  buttonClicked(e) {
+export default function Calculator() {
+  const [calc, setValues] = useState({
+    operation: null,
+    next: null,
+    total: null,
+  });
+  const buttonClicked = (e) => {
     const buttonName = e.target.innerText;
-    const result = calculate(this.state, buttonName);
-    this.setState((previousState) => {
-      const { operation, next, total } = previousState;
-      return {
-        operation: ((result.operation === undefined) ? operation : result.operation),
-        next: ((result.next === undefined) ? next : result.next),
-        total: ((result.total === undefined) ? total : result.total),
-      };
+    const result = calculate(calc, buttonName);
+
+    const { operation, next, total } = calc;
+    setValues({
+      operation: ((result.operation === undefined) ? operation : result.operation),
+      next: ((result.next === undefined) ? next : result.next),
+      total: ((result.total === undefined) ? total : result.total),
     });
-  }
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div id="calculator">
-        <input className="cal-val" value={next || (total || 0)} readOnly />
-        <div className="buttons grid-box">
+  const { next, total } = calc;
+  return (
+    <div id="calculator">
+      <input className="cal-val" value={next || (total || 0)} readOnly />
+      <div className="buttons grid-box">
 
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>+/-</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>%</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>AC</button>
-          <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>÷</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>+/-</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>%</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>AC</button>
+        <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>÷</button>
 
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>7</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>8</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>9</button>
-          <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>x</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>7</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>8</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>9</button>
+        <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>x</button>
 
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>4</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>5</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>6</button>
-          <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>-</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>4</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>5</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>6</button>
+        <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>-</button>
 
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>1</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>2</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>3</button>
-          <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>+</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>1</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>2</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>3</button>
+        <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>+</button>
 
-          <button className="grid-item nill-btn" type="button" onClick={this.buttonClicked}>0</button>
-          <button className="grid-item" type="button" onClick={this.buttonClicked}>.</button>
-          <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>=</button>
-        </div>
+        <button className="grid-item nill-btn" type="button" onClick={this.buttonClicked}>0</button>
+        <button className="grid-item" type="button" onClick={this.buttonClicked}>.</button>
+        <button className="grid-item bg-orange" type="button" onClick={this.buttonClicked}>=</button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default Calculator;
