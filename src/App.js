fd7e748db170ee7d43767e29f3eefb33d7ffb780
sneeu-leeu <sa070197@gmail.com>
2021-08-25 11:05:54 +0200

    update App class to fx and route all switches

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
 import './App.css';
-import Calculator from './components/calculator';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './pages/home';
+import CalcPage from './pages/calculator';
+import Quote from './pages/quotes';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <header
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            padding: '20px',
+          }}
+        >
+          <h1>Math Magicians</h1>
+          <nav>
+            <Link className="p-10 border border-dark mr-20 mt-5 text-decoration-none menu-focus" to="/">Home</Link>
+            <Link className="p-10 border border-dark mr-20 mt-5 text-decoration-none menu-focus" to="/calculator">Calculator</Link>
+            <Link className="p-10 border border-dark mt-5 text-decoration-none" to="/quote">Quote</Link>
+          </nav>
+        </header>
+        <main
+          style={{
+            padding: '0px 70px',
+          }}
+        >
+          <Switch>
+            <Route path="/quote">
+              <Quote />
+            </Route>
+            <Route path="/calculator">
+              <CalcPage />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </main>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
