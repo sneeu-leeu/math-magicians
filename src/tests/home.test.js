71d83c61e695990f9864a6e544eaef23e495f5a4
sneeu-leeu <sa070197@gmail.com>
2021-08-27 16:00:29 +0200

    reset for review

--- src/tests/home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Home from '../pages/home';
-
-test('expect home page to match working snapshot', () => {
-  const homePage = renderer.create(<Home />).toJSON();
-  expect(homePage).toMatchSnapshot();
-});
